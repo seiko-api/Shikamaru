d7aa2317f47fc484e3691d16cee99da0561b711b
Sayu <86679476+seiko-api@users.noreply.github.com>
2022-04-18 06:58:52 +0700

    Update bc.js

--- command/private/bc.js
+++ command/private/bc.js
@@ -8,10 +8,6 @@ module.exports = {
     let data = await store.chats.all()
     for (let i of data) {
     await sock.sendMessage(i.id, { text: `${code}\n\n_*Broadcast Message*_` }, { quoted: msg });
-    await sleep(1000)
+    await new Promise(resolve => setTimeout(resolve, ms));(1000)
     }
-}
-
-const sleep = async (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
 }
